3cbac709a8e1dc07e87300d66b56c536de3e410e
Nam Gideok <namgd1222@gmail.com>
2022-08-08 14:48:29 +0900

    Add TouchText component

--- src/components/mainBackground/MainBackground.tsx
+++ src/components/mainBackground/MainBackground.tsx
@@ -2,7 +2,7 @@ import React, { useReducer, useRef } from "react";
 import "./style.css";
 import inputReducer from "../../features/reducers/inputReducer";
 import FormContainer from "../login/FormContainer";
-import TouchText from "../login/TouchText";
+import TouchText from "../touchText/TouchText";
 
 const MainBackground = () => {
   const [state, dispatch] = useReducer(inputReducer, false);
@@ -10,7 +10,6 @@ const MainBackground = () => {
 
   function handleClick() {
     dispatch({ type: "SET_ON" });
-    console.log(counterRef.current);
   }
   return (
     <div id="background-image" onClick={handleClick}>
---
+++ src/components/touchText/TouchText.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const TouchText = () => {
+  return <h1 className="touch-text">TOUCH THE SCREEN</h1>;
+};
+
+export default TouchText;
