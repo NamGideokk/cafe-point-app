b61a4b1f2ffb86a753cb6e5f9b6ac6b8540f61e5
Nam Gideok <namgd1222@gmail.com>
2022-08-08 14:53:27 +0900

    Add animations.css

--- src/components/signUp/SignUpButton.tsx
+++ src/components/signUp/SignUpButton.tsx
@@ -2,15 +2,7 @@ import React from "react";
 import "./style.css";
 
 const SignUpButton = () => {
-  function handleSignUpButton(e: React.MouseEvent) {
-    e.currentTarget.classList.toggle("move-up");
-  }
-
-  return (
-    <h1 className="sign-up__button" onClick={handleSignUpButton}>
-      회원가입
-    </h1>
-  );
+  return <h1 className="sign-up__button">회원가입</h1>;
 };
 
 export default SignUpButton;
