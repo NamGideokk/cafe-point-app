07df1d48c986fa4177e4c0ccf1d4c09e1e411da5
Nam Gideok <namgd1222@gmail.com>
2022-08-10 15:12:13 +0900

    Add EarnPointContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import EarnPointContainer from "./components/earnPoint/EarnPointContainer";
 import FormContainer from "./components/login/FormContainer";
 import SignUpForm from "./components/signUp/SignUpForm";
 import Home from "./pages/Home";
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<FormContainer />} />
       <Route path="/signup" element={<SignUpForm />} />
+      <Route path="/earnpoint" element={<EarnPointContainer />} />
     </Routes>
   );
 }
