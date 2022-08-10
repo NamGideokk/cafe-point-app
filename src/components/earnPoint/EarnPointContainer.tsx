5d8b93ccd74ad6c4c94a08ad19a04e59384c2583
Nam Gideok <namgd1222@gmail.com>
2022-08-10 22:24:51 +0900

    Add html tag article

--- src/components/earnPoint/EarnPointContainer.tsx
+++ src/components/earnPoint/EarnPointContainer.tsx
@@ -4,35 +4,41 @@ import { ImArrowRight2 } from "react-icons/im";
 
 const EarnPointContainer = () => {
   return (
-    <form className="container items-margin">
-      <label className="amount-paid__field container-width">
-        결제금액
-        <input type="text" name="amount-paid" className="amount-paid__input" />
-        <span id="won">원</span>
-      </label>
+    <article className="article">
+      <form className="container items-margin">
+        <label className="amount-paid__field container-width">
+          결제금액
+          <input
+            type="text"
+            name="amount-paid"
+            className="amount-paid__input"
+          />
+          <span id="won">원</span>
+        </label>
 
-      <div className="check-box__field container-width">
-        <h3>포인트를</h3>
-        <div className="label__container">
-          <label>
-            <input type="radio" name="method" value="earn" checked />
-            <p>적립할게요</p>
-          </label>
-          <label>
-            <input type="radio" name="method" id="redeem" />
-            <p>사용할게요</p>
-          </label>
+        <div className="check-box__field container-width">
+          <h3>포인트를</h3>
+          <div className="label__container">
+            <label>
+              <input type="radio" name="method" value="earn" checked />
+              <p>적립할게요</p>
+            </label>
+            <label>
+              <input type="radio" name="method" id="redeem" />
+              <p>사용할게요</p>
+            </label>
+          </div>
         </div>
-      </div>
 
-      <div className="point__field container-width">
-        <h2>적립될 포인트는 325p 입니다.</h2>
-        <h1>
-          2,000 P <ImArrowRight2 id="arrow-right__icon" />{" "}
-          <mark id="points-after-earning">2,328 P</mark>
-        </h1>
-      </div>
-    </form>
+        <div className="point__field container-width">
+          <h2>적립될 포인트는 325p 입니다.</h2>
+          <h1>
+            2,000 P <ImArrowRight2 id="arrow-right__icon" />{" "}
+            <mark id="points-after-earning">2,328 P</mark>
+          </h1>
+        </div>
+      </form>
+    </article>
   );
 };
 
--- src/components/signUp/SignUpFormContainer.tsx
+++ src/components/signUp/SignUpFormContainer.tsx
@@ -13,39 +13,41 @@ const SignUpFormContainer = () => {
     navi("/signin");
   }
   return (
-    <form className="sign-up__form container" onSubmit={handleSubmit}>
-      <div>
-        <div className="label__container">
-          <span className="input__label">휴대폰 번호</span>
-          <span className="guide">번호만 입력해 주세요</span>
+    <article className="article">
+      <form className="sign-up__form container" onSubmit={handleSubmit}>
+        <div>
+          <div className="label__container">
+            <span className="input__label">휴대폰 번호</span>
+            <span className="guide">번호만 입력해 주세요</span>
+          </div>
+          <input
+            type="text"
+            name="phoneNumber"
+            className="sign-up__input"
+            id=""
+            maxLength={11}
+          />
+          <div className="label__container">
+            <span className="input__label">이름</span>
+            <span className="guide">2~6자로 입력해 주세요</span>
+          </div>
+          <input
+            type="text"
+            name="name"
+            className="sign-up__input"
+            maxLength={6}
+          />
         </div>
-        <input
-          type="text"
-          name="phoneNumber"
-          className="sign-up__input"
-          id=""
-          maxLength={11}
-        />
-        <div className="label__container">
-          <span className="input__label">이름</span>
-          <span className="guide">2~6자로 입력해 주세요</span>
+        <div>
+          <button type="submit" className="button-st1">
+            가입하기
+          </button>
+          <button type="button" className="button-st1" onClick={handleGoBack}>
+            뒤로가기
+          </button>
         </div>
-        <input
-          type="text"
-          name="name"
-          className="sign-up__input"
-          maxLength={6}
-        />
-      </div>
-      <div>
-        <button type="submit" className="button-st1">
-          가입하기
-        </button>
-        <button type="button" className="button-st1" onClick={handleGoBack}>
-          뒤로가기
-        </button>
-      </div>
-    </form>
+      </form>
+    </article>
   );
 };
 
