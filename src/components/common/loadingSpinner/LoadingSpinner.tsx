ab47b73f0a38c72efc1e33027e9c64664ffbd7b7
Nam Gideok <namgd1222@gmail.com>
2022-08-13 00:02:36 +0900

    Add LoadingSpinner Component

---
+++ src/components/common/loadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./loadingSpinner.css";
+
+const LoadingSpinner = () => {
+  return (
+    <>
+      <div className="circle"></div>
+      <div className="circle2"></div>
+    </>
+  );
+};
+
+export default LoadingSpinner;
--- src/components/login/FormContainer.tsx
+++ src/components/login/FormContainer.tsx
@@ -19,10 +19,14 @@ const FormContainer = () => {
     navi("/point");
   }
 
+  function handleSignUpButton() {
+    navi("/signup");
+  }
+
   return (
     <article className="article">
       <form className="container" onSubmit={submitPhoneNumber}>
-        <div className="input__field">
+        <section className="input__field">
           <input
             type="text"
             maxLength={11}
@@ -31,18 +35,21 @@ const FormContainer = () => {
             placeholder="휴대폰 번호 11자리를 입력해 주세요 (숫자만)"
             ref={firstNumber}
           />
-        </div>
-        <div className="button__field">
+        </section>
+        <section className="button__field">
           <Button
             type="submit"
             title="포인트 적립"
             onclickEvent={handlePointEarn}
           />
           <Button type="button" title="포인트 조회" />
-        </div>
-        <NavLink to="/signup">
-          <Button id="sign-up__button" type="button" title="회원가입" />
-        </NavLink>
+        </section>
+        <Button
+          id="sign-up__button"
+          type="button"
+          title="회원가입"
+          onclickEvent={handleSignUpButton}
+        />
       </form>
     </article>
   );
