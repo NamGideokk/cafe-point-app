595a61df5792fedfbc634d772c5efe541fac4dce
Nam Gideok <namgd1222@gmail.com>
2022-08-14 00:08:26 +0900

    Add input focus styles effect, edit semantic tag in SignUpFormContainer

--- src/components/signUp/SignUpFormContainer.tsx
+++ src/components/signUp/SignUpFormContainer.tsx
@@ -1,42 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./signUpFormContainer.css";
 
 const SignUpFormContainer = () => {
   const navi = useNavigate();
 
+  useEffect(() => {}, []);
+
+  // 가입하기 버튼 클릭
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
   }
 
+  // 뒤로가기 버튼 클릭
   function handleGoBack() {
     navi("/signin");
   }
+
   return (
     <article className="article">
       <form className="sign-up__form container" onSubmit={handleSubmit}>
         <main>
-          <div className="label__container">
-            <label className="input__label">휴대폰 번호</label>
-            <span className="guide">번호만 입력해 주세요</span>
-          </div>
-          <input
-            id="phoneNumber__input"
-            type="text"
-            name="phoneNumber"
-            className="sign-up__input"
-            maxLength={11}
-          />
-          <div className="label__container">
-            <label className="input__label">이름</label>
-            <span className="guide">2~6자로 입력해 주세요</span>
-          </div>
-          <input
-            type="text"
-            name="name"
-            className="sign-up__input"
-            maxLength={6}
-          />
+          <section className="phone-number__section">
+            <div className="label__container">
+              <label className="input__label">휴대폰 번호</label>
+              <span className="guide phone-guide">번호만 입력해 주세요</span>
+            </div>
+            <input
+              id="phone-number__input"
+              type="text"
+              name="phoneNumber"
+              className="sign-up__input"
+              maxLength={11}
+              autoFocus
+            />
+          </section>
+          <section className="name__section">
+            <div className="label__container">
+              <label className="input__label">이름</label>
+              <span className="guide name-guide">2~6자로 입력해 주세요</span>
+            </div>
+            <input
+              type="text"
+              name="name"
+              className="sign-up__input"
+              maxLength={6}
+            />
+          </section>
         </main>
         <section>
           <button type="submit" className="button-st1">
