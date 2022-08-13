a4f7546b0e90b1b44f61474ae95f35325be85850
Nam Gideok <namgd1222@gmail.com>
2022-08-13 23:25:06 +0900

    Edit semantic tag in SignUpFormContainer

--- src/components/signUp/SignUpFormContainer.tsx
+++ src/components/signUp/SignUpFormContainer.tsx
@@ -15,20 +15,20 @@ const SignUpFormContainer = () => {
   return (
     <article className="article">
       <form className="sign-up__form container" onSubmit={handleSubmit}>
-        <div>
+        <main>
           <div className="label__container">
-            <span className="input__label">휴대폰 번호</span>
+            <label className="input__label">휴대폰 번호</label>
             <span className="guide">번호만 입력해 주세요</span>
           </div>
           <input
+            id="phoneNumber__input"
             type="text"
             name="phoneNumber"
             className="sign-up__input"
-            id=""
             maxLength={11}
           />
           <div className="label__container">
-            <span className="input__label">이름</span>
+            <label className="input__label">이름</label>
             <span className="guide">2~6자로 입력해 주세요</span>
           </div>
           <input
@@ -37,15 +37,15 @@ const SignUpFormContainer = () => {
             className="sign-up__input"
             maxLength={6}
           />
-        </div>
-        <div>
+        </main>
+        <section>
           <button type="submit" className="button-st1">
             가입하기
           </button>
           <button type="button" className="button-st1" onClick={handleGoBack}>
             뒤로가기
           </button>
-        </div>
+        </section>
       </form>
     </article>
   );
