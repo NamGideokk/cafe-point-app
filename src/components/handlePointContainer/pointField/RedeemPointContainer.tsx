20cee5d9b100dcd11789b1f24eeda54ac9929b09
Nam Gideok <namgd1222@gmail.com>
2022-08-14 09:45:47 +0900

    Edit point Styles in pointField

--- src/components/handlePointContainer/pointField/RedeemPointContainer.tsx
+++ src/components/handlePointContainer/pointField/RedeemPointContainer.tsx
@@ -7,7 +7,9 @@ import { PointProps } from "./EarnPointContainer";
 const RedeemPointContainer = ({ point, prevPoint, afterPoint }: PointProps) => {
   return (
     <div className="point__field container-width">
-      <p className="first__p">사용될 포인트는 {point} P 입니다.</p>
+      <p className="first__p">
+        사용될 포인트는 <mark className="point__mark">{point} P</mark> 입니다.
+      </p>
       <p className="second__p">
         {prevPoint} P <ImArrowRight2 id="arrow-right__icon" />{" "}
         <mark id="points-after-redeem">{afterPoint} P</mark>
