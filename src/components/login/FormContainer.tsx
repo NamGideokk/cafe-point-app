f7c772eb24a6acbd150a8d1a087055e60dc6e328
Nam Gideok <namgd1222@gmail.com>
2022-08-14 10:19:09 +0900

    Add phone number validation

--- src/components/login/FormContainer.tsx
+++ src/components/login/FormContainer.tsx
@@ -1,15 +1,32 @@
-import React, { useEffect, useRef } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../common/button/Button";
 import "./formContainer.css";
+import { BsCheckLg } from "react-icons/bs";
+
+type PhoneNumber = {
+  value: string; // 휴대폰 번호 입력값
+  regResult: boolean; // 입력값 정규표현식 검사 통과 여부
+};
 
 const FormContainer = () => {
   const navi = useNavigate();
-  const firstNumber = useRef<HTMLInputElement>(null);
+  const [phoneNumber, setPhoneNumber] = useState<PhoneNumber>({
+    value: "",
+    regResult: false,
+  });
 
-  useEffect(() => {
-    firstNumber.current?.focus();
-  }, []);
+  const phoneReg = /[\d]{11}/g;
+
+  // 휴대폰 번호 입력
+  function handlePhoneNumber(e: React.FormEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value;
+    const regResult = phoneReg.test(value);
+
+    regResult
+      ? setPhoneNumber({ value: value, regResult: true })
+      : setPhoneNumber({ value: value, regResult: false });
+  }
 
   // 포인트 적립 버튼 클릭
   function handlePointEarn(e: React.FormEvent) {
@@ -31,16 +48,22 @@ const FormContainer = () => {
     <article className="article">
       <form className="container" onSubmit={handlePointEarn}>
         <main className="login__field">
-          <p className="login-phone-guide">
-            휴대폰 번호 11자리를 입력해 주세요 (숫자만)
-          </p>
+          {phoneNumber.regResult ? (
+            <BsCheckLg id="checking__icon" />
+          ) : (
+            <p className="login-phone-guide">
+              휴대폰 번호 11자리를 입력해 주세요 (숫자만)
+            </p>
+          )}
           <section className="input__field">
             <input
               type="text"
               maxLength={11}
               name="phone-number"
               className="phone-number__input"
-              ref={firstNumber}
+              value={phoneNumber.value}
+              onChange={handlePhoneNumber}
+              autoFocus
             />
           </section>
         </main>
